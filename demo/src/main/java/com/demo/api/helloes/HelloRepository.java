3fbcba231afb6866d027e45185f228f4e17e5c06
ParkJinwon1025 <jinwon2812@ubisam.com>
2026-03-03 18:05:49 +0900

    [2026년 3월 3일] 세미나 코드 등록

--- demo/src/main/java/com/demo/api/helloes/HelloRepository.java
+++ demo/src/main/java/com/demo/api/helloes/HelloRepository.java
@@ -7,13 +7,13 @@ import com.demo.domain.Hello;
 import io.u2ware.common.data.jpa.repository.RestfulJpaRepository;
 
 // 2026-03-03 이전
-// context1(), context2() 실행할 때에 주석 풀어야 함.
+// ApplicationTests 파일의 context1(), context2() 테스트할 때에 주석 풀어야 함.
 // public interface HelloRepository extends JpaRepository<Hello, Long> {
 
 // }
 
 // 2026-03-03 이후
-// context3() 수정할 때 주석 풀어야 함. 
+// ApplicationTests 파일의 context3() 함수 테스트할 때 주석 풀어야 함. 
 public interface HelloRepository extends RestfulJpaRepository<Hello, Long> {
 
 }
